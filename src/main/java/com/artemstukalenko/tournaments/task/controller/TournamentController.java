acd2598781b2bbd64926c30610e4b338900d718d
Artem Stukalenko <stukalenko.artem@gmail.com>
2021-10-07 14:20:41 +0300

    Wrote tournaments controller

--- src/main/java/com/artemstukalenko/tournaments/task/controller/TournamentController.java
+++ src/main/java/com/artemstukalenko/tournaments/task/controller/TournamentController.java
@@ -1,9 +1,12 @@
 package com.artemstukalenko.tournaments.task.controller;
 
+import com.artemstukalenko.tournaments.task.entity.Tournament;
 import com.artemstukalenko.tournaments.task.service.TournamentService;
+import com.artemstukalenko.tournaments.task.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,12 +16,52 @@ public class TournamentController {
     @Autowired
     private TournamentService tournamentService;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
     public String getAllTournaments(Model model) {
 
         model.addAttribute("allTournaments", tournamentService.getAllTournaments());
 
         return "tournaments-page.html";
+    }
+
+    @RequestMapping("/addTournament")
+    public String getAddTournamentForm(Model model) {
+
+        model.addAttribute("tournament", new Tournament());
+        model.addAttribute("allUsers", userService.getAllUsers());
+
+        return "tournament-form.html";
+    }
+
+    @RequestMapping("/commitTournament")
+    public String commitTournament(Tournament tournament) {
+
+        tournament.setUser(userService.findUserById(tournament.getUser().getUserId()));
+
+        tournamentService.addOrUpdateTournament(tournament);
+
+        return "forward:/tournaments/";
+    }
+
+    @RequestMapping("/deleteTournament/{id}")
+    public String deleteTournament(@PathVariable("id") int idToDelete) {
+
+        tournamentService.deleteTournamentById(idToDelete);
+
+        return "forward:/tournaments/";
+    }
+
+    @RequestMapping("/updateTournament/{id}")
+    public String getFormToUpdateTournament(@PathVariable("id") int idToUpdate,
+                                            Model model) {
+
+        model.addAttribute("tournament", tournamentService.findTournamentById(idToUpdate));
+        model.addAttribute("allUsers", userService.getAllUsers());
+
+        return "tournament-form.html";
     }
 
 }
