816f1fd362d7700eecdcc89f8f7eac1adef4905e
Artem Stukalenko <stukalenko.artem@gmail.com>
2021-10-13 12:02:54 +0300

    Styled homepage

--- src/main/java/com/artemstukalenko/tournaments/task/controller/MainController.java
+++ src/main/java/com/artemstukalenko/tournaments/task/controller/MainController.java
@@ -1,29 +1,16 @@
 package com.artemstukalenko.tournaments.task.controller;
 
-import com.artemstukalenko.tournaments.task.entity.UserRole;
-import com.artemstukalenko.tournaments.task.service.PlayerService;
-import com.artemstukalenko.tournaments.task.service.UserRoleService;
-import com.artemstukalenko.tournaments.task.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 public class MainController {
-
-    @Autowired
-    private UserRoleService userRoleService;
-
-    @Autowired
-    private UserService userService;
 
     @RequestMapping("/")
     public String getHomepage(Model model) {
 
-        return "homepage.html";
+        return "homepage-with-bootstrap.html";
 
     }
 
